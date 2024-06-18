002c13e7bfed1508b909a25c26f392b5dc790be2
SunJacques <jacques.sun10@gmail.com>
2024-06-19 00:23:29 +0200

    Adding DP

--- Week5_Dynamic_programming/DNA.cpp
+++ Week5_Dynamic_programming/DNA.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <vector>
-#include <sstream>
+#include <climits>
 using namespace std;
 typedef unsigned long long ull;
 
@@ -8,10 +8,9 @@ int main(){
     int N;
     // string line;
     cin >> N;
-    long long int INF = 99999999999;
-    vector<long long int> L(N);
-    vector<long long int> prefix_sum(N+1,0);
-    vector<vector<long long int> > dp(N,vector<long long int>(N,0));
+    vector<unsigned long long> L(N);
+    vector<unsigned long long> prefix_sum(N+1,0);
+    vector<vector<unsigned long long> > dp(N,vector<unsigned long long>(N,0));
     
     // getline(cin, line);
 
@@ -31,20 +30,13 @@ int main(){
     for (int length = 2; length <= N; length++){
         for (int i = 0; i <= N - length; i++){
             int j = i + length - 1;
-            dp[i][j] = INF;
+            dp[i][j] = ULLONG_MAX;
             for (int k = i; k < j; k++){
                 dp[i][j] = min(dp[i][j], dp[i][k] + dp[k+1][j] + prefix_sum[j+1] - prefix_sum[i]);
             }
         }
     }
 
-    for (int i = 0; i < N; i++){
-        for (int j = 0; j < N; j++){
-            cout << dp[i][j] << " ";
-        }
-        cout << endl;
-    }
-
-    cout << dp[0][N - 1] << endl;
+    printf("%llu \n", dp[0][N - 1]);
     return 0;
 }
